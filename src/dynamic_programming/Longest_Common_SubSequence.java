b4a443df85e6cc0672185f860054896a5280ab1a
liupei1988 <461711602@qq.com>
2020-08-21 12:05:07 +0800

    最长公共子序列 最长公共子串

---
+++ src/dynamic_programming/Longest_Common_SubSequence.java
@@ -0,0 +1,10 @@
+package dynamic_programming;
+
+/**
+ * 给定两个字符串str1、str2，求两个字符串的最长公共子序列
+ * 子序列：保持字符相对顺序，可以不连续
+ * 模型：一个样本做行一个样本做列的对应模型
+ */
+public class Longest_Common_SubSequence {
+
+}
---
+++ src/dynamic_programming/Longest_Common_SubString.java
@@ -0,0 +1,10 @@
+package dynamic_programming;
+
+/**
+ * 给定两个字符串str1、str2，求两个字符串的最长公共子串
+ * 子串：必须连续
+ * 模型：一个样本做行一个样本做列的对应模型
+ */
+public class Longest_Common_SubString {
+
+}

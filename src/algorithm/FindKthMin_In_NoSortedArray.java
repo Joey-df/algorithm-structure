031b2872ea99ea557bbf0d031162bee3f09f32f3
liupei1988 <461711602@qq.com>
2020-08-22 12:56:54 +0800

    解决无序数组上找第k小的数: 使用大根堆

--- src/algorithm/FindKthMin_In_NoSortedArray.java
+++ src/algorithm/FindKthMin_In_NoSortedArray.java
@@ -1,10 +1,14 @@
 package algorithm;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * 在无序数组中找出第k小的数
  */
 public class FindKthMin_In_NoSortedArray {
 
+    //方法一：改写快排，时间复杂度O(N)
     //在arr[L...R]范围上找位于index位置的数
     //如果排序的话，不是真的排序
     //潜台词：index一定在L...R上
@@ -49,9 +53,32 @@ public class FindKthMin_In_NoSortedArray {
         return new int[]{less + 1, more - 1};
     }
 
+    //方法二：使用大根堆  堆的大小为k  返回堆顶的元素，就是第k小的
+    //在无序数组中找第k小的数
+    //复杂度o(N*logN)
+    public static int method2(int arr[], int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1; //降序
+            }
+        });
+        for (int i = 0; i < k; i++) {
+            heap.add(arr[i]);
+        }
+        for (int i = k; i < arr.length; i++) {
+            if (heap.peek() > arr[i]) { //如果新进来的元素 比 堆顶元素小的话 进行处理
+                heap.poll(); //移除堆顶元素
+                heap.add(arr[i]);
+            }
+        }
+        return heap.peek();
+    }
+
     public static void main(String[] args) {
         int arr[] = {7, 2, 3, 100, 5, 99, 0, 3, 4, 6, 5};
-        int k = 9;
+        int k = 5;
         System.out.println(findKth(arr, 0, arr.length - 1, k - 1));
+        System.out.println(method2(arr, k));
     }
 }

55f460069caf6016a07332a5e5457d0cd4dba0e0
liupei <liupei@yidian-inc.com>
2020-09-15 15:41:41 +0800

    Problem518_Coin_Change_2 找零钱的方法数 暴力递归->动态规划->优化枚举行为

--- src/dynamic_programming/left_to_right/Problem518_Coin_Change_2.java
+++ src/dynamic_programming/left_to_right/Problem518_Coin_Change_2.java
@@ -4,9 +4,9 @@ package dynamic_programming.left_to_right;
  * You are given coins of different denominations and a total amount of money.
  * Write a function to compute the number of combinations that make up that amount.
  * You may assume that you have infinite number of each kind of coin.
- * <p>
+ *
  * Example 1:
- * <p>
+ *
  * Input: amount = 5, coins = [1, 2, 5]
  * Output: 4
  * Explanation: there are four ways to make up the amount:
@@ -15,25 +15,23 @@ package dynamic_programming.left_to_right;
  * 5=2+1+1+1
  * 5=1+1+1+1+1
  * Example 2:
- * <p>
+ *
  * Input: amount = 3, coins = [2]
  * Output: 0
  * Explanation: the amount of 3 cannot be made up just with coins of 2.
  * Example 3:
- * <p>
+ *
  * Input: amount = 10, coins = [10]
  * Output: 1
- * <p>
- * <p>
+ *
  * Note:
- * <p>
+ *
  * You can assume that
- * <p>
+ *
  * 0 <= amount <= 5000
  * 1 <= coin <= 5000
  * the number of coins is less than 500
  * the answer is guaranteed to fit into signed 32-bit integer
- * <p>
  * 背包问题
  * 从左往右的尝试模型
  */

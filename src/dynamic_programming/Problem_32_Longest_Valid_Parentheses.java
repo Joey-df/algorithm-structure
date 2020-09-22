f17ecbcc13639e651077f9bf883c20c451e150b2
liupei <liupei@yidian-inc.com>
2020-09-23 01:26:32 +0800

    add dp problems

---
+++ src/dynamic_programming/Problem_32_Longest_Valid_Parentheses.java
@@ -0,0 +1,19 @@
+package dynamic_programming;
+
+/**
+ * Given a string containing just the characters '(' and ')',
+ * find the length of the longest valid (well-formed) parentheses substring.
+ *
+ * Example 1:
+ *
+ * Input: "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()"
+ * Example 2:
+ *
+ * Input: ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()"
+ */
+public class Problem_32_Longest_Valid_Parentheses {
+}
---
+++ src/dynamic_programming/number_bit/Problem_233_Number_of_Digit_One.java
@@ -0,0 +1,13 @@
+package dynamic_programming.number_bit;
+
+/**
+ * Given an integer n, count the total number of digit 1 appearing in all non-negative integers less than or equal to n.
+ *
+ * Example:
+ *
+ * Input: 13
+ * Output: 6
+ * Explanation: Digit 1 occurred in the following numbers: 1, 10, 11, 12, 13.
+ */
+public class Problem_233_Number_of_Digit_One {
+}

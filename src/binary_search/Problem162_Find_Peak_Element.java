1408140e8a9d0808fb78fc1210bffb9227f9fc9f
liupei <liupei@yidian-inc.com>
2020-09-22 12:21:26 +0800

    Problem162_Find_Peak_Element

--- src/binary_search/Problem162_Find_Peak_Element.java
+++ src/binary_search/Problem162_Find_Peak_Element.java
@@ -6,26 +6,26 @@ import javax.xml.bind.annotation.XmlID;
 
 /**
  * A peak element is an element that is greater than its neighbors.
- * <p>
+ * 
  * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
- * <p>
+ * 
  * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
- * <p>
+ * 
  * You may imagine that nums[-1] = nums[n] = -∞.
- * <p>
+ * 
  * Example 1:
- * <p>
+ * 
  * Input: nums = [1,2,3,1]
  * Output: 2
  * Explanation: 3 is a peak element and your function should return the index number 2.
  * Example 2:
- * <p>
+ * 
  * Input: nums = [1,2,1,3,5,6,4]
  * Output: 1 or 5
  * Explanation: Your function can return either index number 1 where the peak element is 2,
  * or index number 5 where the peak element is 6.
  * Follow up: Your solution should be in logarithmic complexity.
- * 二分差站找局部最大值问题
+ * 二分查找找局部最大值问题
  */
 public class Problem162_Find_Peak_Element {
 

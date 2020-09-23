ca083cdac285c325c1b6690c8f2a9ed114db0210
liupei <liupei@yidian-inc.com>
2020-09-23 10:29:10 +0800

    add dp problems

---
+++ src/dynamic_programming/row_column/Problem_516_Longest_Palindromic_Subsequence.java
@@ -0,0 +1,37 @@
+package dynamic_programming.row_column;
+
+/**
+ * Given a string s, find the longest palindromic subsequence's length in s.
+ * You may assume that the maximum length of s is 1000.
+ * 
+ * Example 1:
+ * Input:
+ * 
+ * "bbbab"
+ * Output:
+ * 4
+ * One possible longest palindromic subsequence is "bbbb".
+ * 
+ * 
+ * Example 2:
+ * Input:
+ * 
+ * "cbbd"
+ * Output:
+ * 2
+ * One possible longest palindromic subsequence is "bb".
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= s.length <= 1000
+ * s consists only of lowercase English letters.
+ * 
+ * 最长回文子序列
+ * 求的是最长回文子序列的长度
+ * 使用行列尝试模型
+ */
+public class Problem_516_Longest_Palindromic_Subsequence {
+
+
+}

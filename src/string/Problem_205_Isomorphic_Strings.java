9a1f2b14d3e142ca73e11f74196730d32ac8e19e
liupei <liupei@yidian-inc.com>
2020-10-09 19:33:03 +0800

    Problem_205_Isomorphic_Strings

---
+++ src/string/Problem_205_Isomorphic_Strings.java
@@ -0,0 +1,78 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given two strings s and t, determine if they are isomorphic.
+ *
+ * Two strings are isomorphic if the characters in s can be replaced to get t.
+ *
+ * All occurrences of a character must be replaced with another character while preserving the order of characters.
+ * No two characters may map to the same character but a character may map to itself.
+ *
+ * Example 1:
+ *
+ * Input: s = "egg", t = "add"
+ * Output: true
+ * Example 2:
+ *
+ * Input: s = "foo", t = "bar"
+ * Output: false
+ * Example 3:
+ *
+ * Input: s = "paper", t = "title"
+ * Output: true
+ * Note:
+ * You may assume both s and t have the same length.
+ */
+public class Problem_205_Isomorphic_Strings {
+
+    private static class Wrap {
+        StringBuilder index;
+        int count;
+
+        public Wrap() {
+        }
+
+        public Wrap(StringBuilder i, int c) {
+            index = i;
+            count = c;
+        }
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        if (s == null || t == null) return false;
+        if (s.length() != t.length()) return false;
+        return process(s.toCharArray(), t.toCharArray());
+    }
+
+    private static boolean process(char[] s, char[] t) {
+        Map<Character, Wrap> map1 = new HashMap<>();
+        for (int i = 0; i < s.length; i++) {
+            if (map1.get(s[i]) != null) {
+                map1.put(s[i], new Wrap(map1.get(s[i]).index.append(i), map1.get(s[i]).count + 1));
+            } else {
+                map1.put(s[i], new Wrap(new StringBuilder(i), 1));
+            }
+
+        }
+        Map<Character, Wrap> map2 = new HashMap<>();
+        for (int i = 0; i < t.length; i++) {
+            if (map2.containsKey(t[i])) {
+                map2.put(t[i], new Wrap(map2.get(t[i]).index.append(i), map2.get(t[i]).count + 1));
+            } else {
+                map2.put(t[i], new Wrap(new StringBuilder(i), 1));
+            }
+        }
+        for (int i = 0; i < s.length; i++) {
+            if (map1.get(s[i]).count != map2.get(t[i]).count
+                    || !map1.get(s[i]).index.toString().equals(map2.get(t[i]).index.toString())) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("abba", "cbbc"));
+    }
+}

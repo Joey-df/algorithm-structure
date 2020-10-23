3515ce2e28b0945fa6a8b15552eacbd387da186e
liupei <liupei@yidian-inc.com>
2020-10-23 17:34:53 +0800

    Problem_76_Minimum_Window_Substring

--- src/slide_window/Problem_76_Minimum_Window_Substring.java
+++ src/slide_window/Problem_76_Minimum_Window_Substring.java
@@ -12,6 +12,49 @@ package slide_window;
  * If there is no such window in S that covers all characters in T, return the empty string "".
  * If there is such window, you are guaranteed that there will always be only one unique minimum window in S.
  */
-public class Problem_76_Minimum_Window_Substring {
 
+//时间复杂度 O(N)
+public class Problem_76_Minimum_Window_Substring {
+    public static String process(String S, String T) {
+        if (S.length() < T.length()) {
+            return "";
+        }
+        char[] str = S.toCharArray();
+        char[] target = T.toCharArray();
+        int[] map = new int[256];//欠账表
+        for (char c : target) {
+            map[c]++;
+        }
+        int all = target.length;//欠账总数
+        int L = 0, R = 0;
+        int minLen = -1, ansL = -1, ansR = -1;
+
+        while (R < str.length) {
+            map[str[R]]--;
+            if (map[str[R]] >= 0) {
+                all--; //map[str[R]]减1之后>=0时，all--
+            }
+            if (all == 0) {
+                while (map[str[L]] < 0) {
+                    map[str[L++]]++;
+                }
+                //收集答案
+                if (minLen == -1 || minLen > R - L + 1) {
+                    minLen = R - L + 1;
+                    ansL = L;
+                    ansR = R;
+                }
+                //尝试下一个位置，重新开始欠账
+                map[str[L++]]++;
+                all++;
+            }
+            R++;
+        }
+        return minLen == -1 ? "" : S.substring(ansL, ansR + 1);
+    }
+
+    public static void main(String[] args) {
+        String S = "ADOBECODEBANC", T = "AB";
+        System.out.println(process(S, T));
+    }
 }

9c73f107383050d49dd7465817e976427cc24ff9
liupei <liupei@yidian-inc.com>
2020-10-23 22:21:39 +0800

    Problem_Longest_Substring_with_At_Most_K_Distinct_Characters

---
+++ src/slide_window/Problem_340_Longest_Substring_with_At_Most_K_Distinct_Characters.java
@@ -0,0 +1,49 @@
+package slide_window;
+
+/**
+ * Given a string, find the length of the longest substring T that contains at most k distinct characters.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "eceba", k = 2
+ * Output: 3
+ * Explanation: T is "ece" which its length is 3.
+ * Example 2:
+ * <p>
+ * Input: s = "aa", k = 1
+ * Output: 2
+ * Explanation: T is "aa" which its length is 2.
+ * 加锁的题
+ */
+public class Problem_340_Longest_Substring_with_At_Most_K_Distinct_Characters {
+
+    public static int process(String s, int k) {
+        if (s == null || "".equals(s) || s.length() == 0 || k <= 0) {
+            return 0;
+        }
+        int R = 0, diff = 0;
+        int ans = 0;
+        char[] str = s.toCharArray();
+        int N = str.length;
+        int[] map = new int[128];//记账表
+        for (int i = 0; i < N; i++) { //尝试每一个开头
+            while (R < N &&
+                    (diff < k || (diff == k && map[str[R]] > 0))) {
+                diff += map[str[R]] == 0 ? 1 : 0;
+                map[str[R++]]++;
+            }
+            //R来到下一个违规的位置
+            ans = Math.max(ans, R - i);
+            //i即将++,需要处理i位置的状态
+            if (map[str[i]] == 1) {
+                diff--;
+            }
+            map[str[i]]--;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(process("aa", 1));
+    }
+}
--- src/slide_window/Problem_Longest_Substring_with_At_Most_Two_Distinct_Characters.java
+++ src/slide_window/Problem_Longest_Substring_with_At_Most_Two_Distinct_Characters.java
@@ -2,18 +2,43 @@ package slide_window;
 
 /**
  * Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: "eceba"
  * Output: 3
  * Explanation: tis "ece" which its length is 3.
  * Example 2:
- *
+ * <p>
  * Input: "ccaabbb"
  * Output: 5
  * Explanation: tis "aabbb" which its length is 5.
  */
 public class Problem_Longest_Substring_with_At_Most_Two_Distinct_Characters {
 
+    public static int process(String s) {
+        int L = 0, R = 0;
+        int diff = 0;
+        int ans = 0;
+        char[] str = s.toCharArray();
+        int N = str.length;
+        int[] map = new int[256];
+        for (; L < N; L++) {
+            while (R < N && (diff < 2 || (diff == 2 && map[str[R]] > 0))) {
+                diff += map[str[R]] == 0 ? 1 : 0;
+                map[str[R++]]++;
+            }
+            //R来到第一个违规的位置
+            ans = Math.max(ans, R - L);
+            //处理L位置(L即将++)
+            diff -= map[str[L]] == 1 ? 1 : 0;
+            map[str[L]]--;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(process("eceba"));
+        System.out.println(process("ccaabbb"));
+    }
 }

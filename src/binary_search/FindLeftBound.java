6b6d2a202e8d1e96bfee657c2ea5de2341f4c503
liupei <liupei@yidian-inc.com>
2020-11-02 11:48:02 +0800

    fix二分

--- src/binary_search/FindLeftBound.java
+++ src/binary_search/FindLeftBound.java
@@ -5,52 +5,30 @@ package binary_search;
  */
 public class FindLeftBound {
 
-    public static int leftBound(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        int left = 0;
-        int right = nums.length; // 注意
-
-        while (left < right) { // 注意
-            int mid = (left + right) / 2;
-            if (target == nums[mid]) { //相等时，别返回，锁定右边界，继续到左边找
-                right = mid;
-            } else if (target > nums[mid]) {
-                left = mid + 1;
-            } else { //target < nums[mid]
-                right = mid;
-            }
-        }
-        return left;
-    }
-
     // 在arr上，找满足>=value的最左位置
     // 如果没找到，L来到R+1的位置
     public static int nearestIndex(int[] arr, int value) {
         if (arr.length == 0) return -1;
         int L = 0;
         int R = arr.length - 1;
+        int index = -1; //记录对号的位置
         while (L <= R) {
             int mid = L + ((R - L) >> 1);
-            if (value == arr[mid]) { //相等时，别返回，锁定右边界，继续到左边找
+            if (arr[mid] >= value) {//中点位置的数大于等于value的话，记录位置，然后去左侧去二分
+                index = mid;
                 R = mid - 1;
-            } else if (value > arr[mid]) {
+            } else {
                 L = mid + 1;
-            } else { //value < arr[mid]
-                R = mid - 1;
             }
         }
-        // 最后要检查 left 越界的情况
-        if (L >= arr.length || arr[L] != value)
-            return -1;
-        return L;
+
+        return index;
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 2, 2, 2, 2, 2, 4, 4, 5, 6, 6, 6, 8, 10, 11};
-//        int arr[] = {2, 3, 5, 7};
-        int target = 60;
+        int arr[] = {1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 5, 7};
+        int target = 20;
         System.out.println("arr.length = " + arr.length);
-        System.out.println("leftBound(arr, " + target + ") = " + leftBound(arr, target));
         System.out.println("nearestIndex(arr, " + target + ") = " + nearestIndex(arr, target));
     }
 }
--- src/binary_search/FindRightBound.java
+++ src/binary_search/FindRightBound.java
@@ -1,53 +1,35 @@
 package binary_search;
 
 /**
- * 在有序数组上，找满足 <=value 的最左位置
+ * 在有序数组上，找满足 <=value 的最右位置
  */
 public class FindRightBound {
 
-    public static int rightBound(int[] nums, int target) {
-        if (nums.length == 0) return -1;
-        int left = 0, right = nums.length;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (target == nums[mid]) { //相等时，别返回，锁定左边界，继续在右边找
-                left = mid + 1;
-            } else if (target > nums[mid]) {
-                left = mid + 1; // 注意
-            } else { //target < nums[mid]
-                right = mid;
-            }
-        }
-        return right - 1; // 注意
-    }
-
     // 在arr上，找满足<=value的最右位置
+    // 如果没找到，R来到-1位置
     public static int nearestIndex(int[] arr, int value) {
         if (arr.length == 0) return -1;
         int L = 0;
         int R = arr.length - 1;
+        int index = -1;
         while (L <= R) {
             int mid = L + ((R - L) >> 1);
-            if (value == arr[mid]) { //相等时，别返回，锁定左边界，继续在右边找
+            if (arr[mid] <= value) {//中点位置的数<=value的话，记录位置，然后去右边二分
+                index = mid;
                 L = mid + 1;
-            } else if (value > arr[mid]) {
-                L = mid + 1;
-            } else { //value < arr[mid]
+            } else {
                 R = mid - 1;
             }
         }
-        // 最后要检查 right 越界的情况
-        if (R < 0 || arr[R] != value)
-            return -1;
-        return R;
+
+        return index;
     }
 
     public static void main(String[] args) {
         int arr[] = {1, 2, 2, 2, 2, 2, 4, 4, 5, 6, 6, 6, 8, 10, 11};
 //        int arr[] = {1, 2, 3, 4};
-        int target = 7;
+        int target = -100;
         System.out.println("arr.length = " + arr.length);
-        System.out.println("rightBound(arr, " + target + ") = " + rightBound(arr, target));
         System.out.println("nearestIndex(arr, " + target + ") = " + nearestIndex(arr, target));
     }
 }

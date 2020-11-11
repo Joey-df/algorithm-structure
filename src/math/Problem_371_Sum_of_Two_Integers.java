dec7871149b2687b7fae7ea92fcfd4a3eeb2d87d
liupei <liupei@yidian-inc.com>
2020-11-11 16:56:15 +0800

    Problem_371_Sum_of_Two_Integers

---
+++ src/math/Problem_371_Sum_of_Two_Integers.java
@@ -0,0 +1,29 @@
+package math;
+
+/**
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: a = 1, b = 2
+ * Output: 3
+ * Example 2:
+ * <p>
+ * Input: a = -2, b = 3
+ * Output: 1
+ */
+public class Problem_371_Sum_of_Two_Integers {
+    public static int getSum(int a, int b) {
+        int sum = 0;
+        while (b != 0) { //b充当进位信息, 当进位信息为0时，sum就是答案
+            sum = a ^ b;
+            b = (a & b) << 1;
+            a = sum;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSum(12, 23));
+    }
+}

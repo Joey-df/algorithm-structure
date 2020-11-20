17e106c4269619fb54792a65ad6a7169b6d9108d
liupei <liupei@yidian-inc.com>
2020-11-20 16:48:41 +0800

    Problem_128_Longest_Consecutive_Sequence

---
+++ src/array/Problem_128_Longest_Consecutive_Sequence.java
@@ -0,0 +1,54 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+ * <p>
+ * Follow up: Could you implement the O(n) solution?
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [100,4,200,1,3,2]
+ * Output: 4
+ * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * Example 2:
+ * <p>
+ * Input: nums = [0,3,7,2,5,8,4,6,0,1]
+ * Output: 9
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ */
+public class Problem_128_Longest_Consecutive_Sequence {
+    // First turn the input into a set of numbers. That takes O(n) and then we can ask in O(1) whether we have a certain number.
+    // Then go through the numbers. If the number x is the start of a streak (i.e., x-1 is not in the set), then test y = x+1, x+2, x+3, ... and stop at the first number y not in the set.
+    // The length of the streak is then simply y-x and we update our global best with that.
+    // Since we check each streak only once, this is overall O(n).
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        int best = 0;
+        for (int x : nums) {
+            if (!set.contains(x - 1)) {
+                int y = x + 1;
+                while (set.contains(y)) {
+                    y++;
+                }
+                best = Math.max(best, y - x);
+            }
+        }
+        return best;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+        System.out.println(longestConsecutive(nums));
+    }
+}

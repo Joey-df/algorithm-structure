a826a68c31fb957cc1ff7712a3549e5307edd69b
liupei <liupei@yidian-inc.com>
2020-11-21 22:58:57 +0800

    Problem_171_Excel_Sheet_Column_Number

---
+++ src/string/Problem_171_Excel_Sheet_Column_Number.java
@@ -0,0 +1,50 @@
+package string;
+
+/**
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * <p>
+ * For example:
+ * <p>
+ * A -> 1
+ * B -> 2
+ * C -> 3
+ * ...
+ * Z -> 26
+ * AA -> 27
+ * AB -> 28
+ * ...
+ * Example 1:
+ * <p>
+ * Input: "A"
+ * Output: 1
+ * Example 2:
+ * <p>
+ * Input: "AB"
+ * Output: 28
+ * Example 3:
+ * <p>
+ * Input: "ZY"
+ * Output: 701
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 7
+ * s consists only of uppercase English letters.
+ * s is between "A" and "FXSHRXW".
+ */
+public class Problem_171_Excel_Sheet_Column_Number {
+    //伪26进制
+    public static int titleToNumber(String s) {
+        char[] str = s.toCharArray();
+        int ans = 0;
+        for (int i = 0; i < str.length; i++) {
+            ans = ans * 26 + ((str[i] - 'A') + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(titleToNumber("AB"));
+    }
+}

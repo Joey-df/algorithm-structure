5d8b62f2d2ed410dba7fdb88f798b6be746890f6
liupei <liupei@yidian-inc.com>
2021-01-28 10:44:09 +0800

    返回单链表第一个入环节点，如果没有返回null

---
+++ src/linked_list/Problem_142_Linked_List_Cycle_II.java
@@ -0,0 +1,34 @@
+package linked_list;
+
+/**
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ */
+public class Problem_142_Linked_List_Cycle_II {
+    //返回链表第一个入环节点，如果没有环，返回null
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        //链表有三个点及以上
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+
+        while (slow != fast) {
+            if (fast.next == null || fast.next.next == null) {
+                return null; //链表无环
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        //走到这里说明slow和fast相遇
+        //slow和fast相遇时，让其中任何一个重新指向head，两个每次走一步，再次相遇的地方就是第一个入环节点
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+}

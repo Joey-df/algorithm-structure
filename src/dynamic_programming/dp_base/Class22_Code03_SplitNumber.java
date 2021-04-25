fa892a864f4a9842b66f013ae5879c7f72cfe823
liupei <liupei@yidian-inc.com>
2021-04-25 22:13:28 +0800

    add problems

---
+++ src/dynamic_programming/dp_base/Class22_Code03_SplitNumber.java
@@ -0,0 +1,16 @@
+package dynamic_programming.dp_base;
+
+/**
+ * 给定一个正数n，求n的裂开方法数，
+ * 规定：后面的数不能比前面的数小
+ * 比如4的裂开方法有：
+ * 1+1+1+1、1+1+2、1+3、2+2、4
+ * 5种，所以返回5
+ *
+ * 带斜率优化的dp
+ *
+ * PS：印度数学家拉马努金整数拆分问题
+ */
+public class Class22_Code03_SplitNumber {
+
+}

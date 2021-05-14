0af7a38e292b0a10c979510fd99823d815a666f9
liupei <liupei@yidian-inc.com>
2021-05-14 20:56:53 +0800

    commit code 子矩阵最大累加和

--- src/coding_for_great_offer/class04/Code02_SubArrayMaxSum.java
+++ src/coding_for_great_offer/class04/Code02_SubArrayMaxSum.java
@@ -6,4 +6,17 @@ package coding_for_great_offer.class04;
  */
 public class Code02_SubArrayMaxSum {
 
+    //遇到子数组
+    //思考问题方式：每个位置结尾如何如何
+    public static int maxSum(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        int ans = arr[0];
+        int pre = arr[0];//i-1位置结尾的最大子数组累加和存在pre中
+        for (int i = 1; i < arr.length; i++) {
+            int cur = arr[i] + (Math.max(pre, 0));
+            ans = Math.max(ans, cur);
+            pre = cur;
+        }
+        return ans;
+    }
 }

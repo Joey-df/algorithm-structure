c0a2647c63fc8a34349823f0a44bcd8b02db0391
liupei <liupei@yidian-inc.com>
2021-05-19 23:30:27 +0800

    commit code

--- src/system_study/class03/Code01_ReverseList.java
+++ src/system_study/class03/Code01_ReverseList.java
@@ -1,4 +1,25 @@
 package system_study.class03;
 
+import linked_list.ListNode;
+
+//反转单链表
+//leetcode 206
+// https://leetcode.com/problems/reverse-linked-list/
 public class Code01_ReverseList {
+
+    public static ListNode reverse(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
 }
--- src/system_study/class03/Code02_DeleteGivenValue.java
+++ src/system_study/class03/Code02_DeleteGivenValue.java
@@ -1,4 +1,30 @@
 package system_study.class03;
 
+import linked_list.ListNode;
+
+//删除链表中等于给定值 val 的所有节点
+//leetcode 203
+//https://leetcode.com/problems/remove-linked-list-elements/
 public class Code02_DeleteGivenValue {
+
+    public static ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        //head来到第一个不是val的节点
+        ListNode cur = head;
+        ListNode pre = head;
+        while (cur != null) {
+            if (cur.val != val) {
+                pre = cur;
+            } else { // cur.val==val
+                pre.next = cur.next;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
 }

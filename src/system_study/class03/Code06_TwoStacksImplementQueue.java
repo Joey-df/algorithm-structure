e7617dc2296cdfb912c5ea70026ebb98e14f1197
liupei <liupei@yidian-inc.com>
2021-05-20 17:51:01 +0800

    commit code

--- src/system_study/class03/Code06_TwoStacksImplementQueue.java
+++ src/system_study/class03/Code06_TwoStacksImplementQueue.java
@@ -1,4 +1,59 @@
 package system_study.class03;
 
+import java.util.Stack;
+
+// 使用两个栈实现队列
+// leetcode 232
+// https://leetcode.com/problems/implement-queue-using-stacks
 public class Code06_TwoStacksImplementQueue {
+
+    public static class MyQueue {
+
+        private Stack<Integer> s1; //push
+        private Stack<Integer> s2; //pop
+
+        /** Initialize your data structure here. */
+        public MyQueue() {
+            s1 = new Stack<>();
+            s2 = new Stack<>();
+        }
+
+        //help函数
+        //s2为空时才能倒，要倒务必一次性倒完
+        private void pushToPop() {
+            if (s2.isEmpty()) {
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+            }
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            pushToPop();
+            s1.push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            pushToPop();
+            if (s2.isEmpty()) {
+                throw new RuntimeException("no element");
+            }
+            return s2.pop();
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            pushToPop();
+            return s2.peek();
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            pushToPop();
+            return s2.isEmpty();
+        }
+    }
+
 }
--- src/system_study/class03/Code07_TwoQueueImplementStack.java
+++ src/system_study/class03/Code07_TwoQueueImplementStack.java
@@ -1,4 +1,75 @@
 package system_study.class03;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+//leetcode 225
+//https://leetcode.com/problems/implement-stack-using-queues
+//使用两个队列实现栈
 public class Code07_TwoQueueImplementStack {
+
+    public static class MyStack {
+
+        private Queue<Integer> data;
+        private Queue<Integer> help;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyStack() {
+            data = new LinkedList<>();
+            help = new LinkedList<>();
+        }
+
+        /**
+         * Push element x onto stack.
+         */
+        public void push(int x) {
+            data.offer(x);
+        }
+
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
+        public int pop() {
+            if (data.isEmpty()) {
+                throw new RuntimeException("no data");
+            }
+            int size = data.size();
+            while (size-- > 1) {
+                help.offer(data.poll());
+            }
+            int ans = data.poll();
+            //data、help互换，每次都保证help为空
+            Queue<Integer> tmp = data;
+            data = help;
+            help = tmp;
+            return ans;
+        }
+
+        /**
+         * Get the top element.
+         */
+        public int top() {
+            int size = data.size();
+            while (size-- > 1) {
+                help.offer(data.poll());
+            }
+            int ans = data.poll();
+            help.offer(ans);
+            //data、help互换，每次都保证help为空
+            Queue<Integer> tmp = data;
+            data = help;
+            help = tmp;
+            return ans;
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         */
+        public boolean empty() {
+            return data.isEmpty();
+        }
+    }
+
 }

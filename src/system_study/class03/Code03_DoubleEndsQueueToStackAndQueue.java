1aeefa074b6e8af4eb8e461c9ebd0f9a6880ccf4
liupei <liupei@yidian-inc.com>
2021-05-20 19:26:47 +0800

    commit code

--- src/system_study/class03/Code03_DoubleEndsQueueToStackAndQueue.java
+++ src/system_study/class03/Code03_DoubleEndsQueueToStackAndQueue.java
@@ -1,4 +1,5 @@
 package system_study.class03;
 
+//双向链表实现栈和队列
 public class Code03_DoubleEndsQueueToStackAndQueue {
 }
--- src/system_study/class03/Code05_GetMinStack.java
+++ src/system_study/class03/Code05_GetMinStack.java
@@ -1,4 +1,49 @@
 package system_study.class03;
 
+import java.util.Stack;
+
+/**
+ * 实现一个特殊的栈，在基本功能的基础上，再实现返回栈中最小元素的功能
+ * 1）pop、push、getMin操作的时间复杂度都是 O(1)。
+ * 2）设计的栈类型可以使用现成的栈结构。
+ */
+//leetcode 115题 https://leetcode.com/problems/min-stack/
 public class Code05_GetMinStack {
+
+    public static class MinStack {
+
+        private Stack<Integer> normal;
+        private Stack<Integer> min;
+
+        /**
+         * initialize your data structure here.
+         */
+        public MinStack() {
+            normal = new Stack<>();
+            min = new Stack<>();
+        }
+
+        public void push(int val) {
+            normal.push(val);
+            if (min.isEmpty()) {
+                min.push(val);
+                return;
+            }
+            int data = Math.min(min.peek(), val);
+            min.push(data);
+        }
+
+        public void pop() {
+            normal.pop();
+            min.pop();
+        }
+
+        public int top() {
+            return normal.peek();
+        }
+
+        public int getMin() {
+            return min.peek();
+        }
+    }
 }

1e2e4a1edccc335c24cac8cc7b6922c09f730e95
liupei <liupei@yidian-inc.com>
2021-08-16 10:56:49 +0800

    commit code

--- src/system_study/class04/Code01_MergeSort.java
+++ src/system_study/class04/Code01_MergeSort.java
@@ -1,12 +1,14 @@
 package system_study.class04;
 
+import system_study.util.ArrayUtil;
+
 /**
  * 归并排序
  * 1）整体是递归，左边排好序+右边排好序+merge让整体有序
  * 2）让其整体有序的过程里用了排外序方法
  * 3）利用master公式来求解时间复杂度
  * 4）当然可以用非递归实现
- *
+ * <p>
  * 归并排序复杂度
  * T(N) = 2*T(N/2) + O(N^1)
  * 根据master可知时间复杂度为O(N*logN)
@@ -14,4 +16,97 @@ package system_study.class04;
  * 归并排序的实质是把比较行为变成了有序信息并传递，比O(N^2)的排序快
  */
 public class Code01_MergeSort {
+
+    // 递归方法实现
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+    // 请把arr[L..R]排有序
+    // l...r N
+    // T(N) = 2 * T(N / 2) + O(N)
+    // O(N * logN)
+    public static void process(int[] arr, int L, int R) {
+        if (L == R) { // base case
+            return;
+        }
+        int mid = L + ((R - L) >> 1);
+        process(arr, L, mid);
+        process(arr, mid + 1, R);
+        merge(arr, L, mid, R);
+    }
+
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        // 要么p1越界了，要么p2越界了
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+    }
+
+    // 非递归方法实现
+    public static void mergeSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        // 步长
+        int mergeSize = 1;
+        while (mergeSize < N) { // log N
+            // 当前左组的，第一个位置
+            int L = 0;
+            while (L < N) {
+                if (mergeSize >= N - L) {
+                    break;
+                }
+                int M = L + mergeSize - 1;
+                int R = M + Math.min(mergeSize, N - M - 1);
+                merge(arr, L, M, R);
+                L = R + 1;
+            }
+            // 防止溢出
+            if (mergeSize > N / 2) {
+                break;
+            }
+            mergeSize <<= 1;
+        }
+    }
+
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = ArrayUtil.generateRandomArray(maxSize, maxValue);
+            int[] arr2 = ArrayUtil.copyArray(arr1);
+            mergeSort1(arr1);
+            mergeSort2(arr2);
+            if (!ArrayUtil.isEqual(arr1, arr2)) {
+                System.out.println("出错了！");
+                ArrayUtil.printArray(arr1);
+                ArrayUtil.printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
 }

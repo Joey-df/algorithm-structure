4f2536b183bb85708fa635d396dd624ed24dcdf0
liupei <liupei@yidian-inc.com>
2021-08-16 11:15:35 +0800

    commit code

---
+++ src/system_study/util/DoubleListNode.java
@@ -0,0 +1,12 @@
+package system_study.util;
+
+//双向链表节点
+public class DoubleListNode {
+    private int value;
+    private DoubleListNode last;
+    private DoubleListNode next;
+
+    public DoubleListNode(int v) {
+        value = v;
+    }
+}

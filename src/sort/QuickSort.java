2697968cabe5eabcfc40c1ac4358960706f62b5d
liupei <liupei@yidian-inc.com>
2023-02-14 20:39:59 +0800

    mod

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -78,7 +78,7 @@ public class QuickSort {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
-        System.out.println("--------------");
+        System.out.println("----------------");
         quickSort(arr, 0, arr.length - 1);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
